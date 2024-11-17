34f1adb475b9ce0f42edd1ee0f1fcb037d0d6df5
Steven Bister <stevebister@gmail.com>
2024-11-17 16:41:40 +0000

    add method to get the default list

--- src/lib/server/db/List.ts
+++ src/lib/server/db/List.ts
@@ -1,3 +1,4 @@
+import { and, eq } from 'drizzle-orm';
 import type { DbClient } from './connection';
 import { list } from './schema/list';
 import { TableCommon } from './TableCommon';
@@ -5,5 +6,12 @@ import { TableCommon } from './TableCommon';
 export class List extends TableCommon<typeof list> {
 	constructor(db: DbClient) {
 		super(db, list);
+	}
+
+	async getDefault(currentUserId: string) {
+		return await this.db
+			.select()
+			.from(this.schema)
+			.where(and(eq(this.schema.createdBy, currentUserId), eq(this.schema.name, 'Default')));
 	}
 }
