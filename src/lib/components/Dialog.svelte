2ba572be5965f5a7c79bbcaf6e470a2ab70f031c
Steven Bister <stevebister@gmail.com>
2024-11-17 16:41:00 +0000

    make dialog bindable

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
 	import type { HTMLDialogAttributes } from 'svelte/elements';
-
-	let dialog: HTMLDialogElement;
-
 	interface Props extends HTMLDialogAttributes {
 		triggerText: string;
+		dialog?: HTMLDialogElement;
 	}
 
-	let { children, triggerText }: Props = $props();
+	let { children, triggerText, dialog = $bindable(), ...rest }: Props = $props();
 </script>
 
-<dialog bind:this={dialog}>
-	<button onclick={() => dialog.close()}>close</button>
+<dialog bind:this={dialog} {...rest}>
+	<button onclick={() => dialog?.close()}>close</button>
 
 	{@render children?.()}
 </dialog>
 
-<button onclick={() => dialog.showModal()}>{triggerText}</button>
+<button onclick={() => dialog?.showModal()}>{triggerText}</button>
